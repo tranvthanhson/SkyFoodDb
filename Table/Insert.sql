6cff7ac68faef07cf2b00ad28f0205ac03676aab
Son, Tran Vu Thanh <tranvthanhson@gmail.com>
2017-09-29 23:49:24 +0700

    [23:49 29/09/20217]

--- Table/Insert.sql
+++ Table/Insert.sql
@@ -1,4 +1,7 @@
 -- Insert value to Account Table
 
-INSERT INTO ACCOUNT (USERNAME, PASSWORD, FIRST_NAME, LAST_NAME, FULL_NAME, ROLE
-	VALUES ("Admin", "Admin", "Admin", "Admin", "Admin", 0);
+INSERT INTO ACCOUNT (USERNAME, PASSWORD, FIRST_NAME, LAST_NAME, FULL_NAME, ROLE) VALUES
+	("Admin", "Admin", "Admin", "Admin", "Admin", 0),
+	("tranvthanhson", "1", "Son", "Tran", "Tran Vu Thanh Son", 1),
+	("ovquang", "2", "Quang", "Ong", "Ong Van Quang", 1),
+	("tqdat", "3", "Dat", "Tran", "Tran Quoc Dat", 1);
