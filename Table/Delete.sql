e052a1a6568c1b370e8f047410a290a8139f3710
Son, Tran Vu Thanh <tranvthanhson@gmail.com>
2017-09-29 23:28:12 +0700

    [23:28 29/09/20217]

---
+++ Table/Delete.sql
@@ -0,0 +1,23 @@
+-- Delete account table value
+DELETE FROM ACCOUNT;
+
+-- Delete save table value
+DELETE FROM SAVE;
+
+-- Delete comment table value
+DELETE FROM COMMENT;
+
+-- Delete rate table value
+DELETE FROM RATE;
+
+-- Delete type table value
+DELETE FROM TYPE;
+
+-- Delete shop table value
+DELETE FROM SHOP;
+
+-- Delete type_shop table value
+DELETE FROM TYPE_SHOP;
+
+-- Delete foods table value
+DELETE FROM FOODS;
---
+++ Table/Insert.sql
@@ -0,0 +1,4 @@
+-- Insert value to Account Table
+
+INSERT INTO ACCOUNT (USERNAME, PASSWORD, FIRST_NAME, LAST_NAME, FULL_NAME, ROLE
+	VALUES ("Admin", "Admin", "Admin", "Admin", "Admin", 0);
